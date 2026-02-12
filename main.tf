86202adb4f256176090b573e9359040577511270
Chibuzor Victor Umezuruike <41339694+umezvictor@users.noreply.github.com>
2026-02-12 16:01:28 +0200

    added storage account

--- main.tf
+++ main.tf
@@ -34,3 +34,19 @@ resource "azurerm_linux_web_app" "webapp" {
     }
   }
 }
+
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "webapp1147"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Create a storage container
+resource "azurerm_storage_container" "app_container" {
+  name                  = "appcontainer"
+  container_access_type = "private"
+  storage_account_name  = azurerm_storage_account.sa.name
+}
